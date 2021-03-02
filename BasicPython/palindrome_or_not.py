18973801b7f2dfcea6c2a06d4b1cd23765a44b5a
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-03-02 16:40:14 +0530

    added method 2

--- BasicPython/palindrome_or_not.py
+++ BasicPython/palindrome_or_not.py
@@ -1,7 +1,7 @@
 #Program to check a String is palindrome or not?
 #eg. RadaR, Dad
 
-def palindrome(str1):
+def palindrome1(str1):
     l = 0 #lower Index
     h = len(str1) - 1 #upper Index
     str1 = str1.lower()
@@ -13,7 +13,19 @@ def palindrome(str1):
             h -= 1
     return True
 
+
+def palindrome2(str1):
+    str1 = str1.lower()
+    lst1 = list(str1)
+    lst2 = lst1[::-1]
+    if lst1 == lst2:
+        return True
+    else:
+        return False
+
+
 #driver code
 inputStr = input('Please input a string.\n')
-print('Is String palindrome :', palindrome(inputStr))
+print('Is String palindrome by method 1 :',palindrome1(inputStr)) #calling method one
+print('Is String palindrome by method 2:',palindrome2(inputStr)) #calling method two
 
